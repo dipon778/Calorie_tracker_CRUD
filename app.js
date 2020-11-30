b2ba0cbad241b68aeb52116cfea3af1aa5d32cfa
Dipon <dipon@localhost.localdomain>
2020-11-30 22:24:01 +0600

    add data to data structure

--- app.js
+++ app.js
@@ -23,6 +23,24 @@ const ItemCtrl = (function(){
         getItems: function(){
             return data.items;
         },
+        addItem:function(name,calories){
+            //Create id
+            let ID;
+            if(data.items.length>0){
+                ID=data.items[data.items.length-1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            //Calories to number
+            calories =parseInt(calories);
+
+            //Create new item
+            newItem = new Item(ID,name,calories);
+            data.items.push(newItem); 
+
+            return newItem;
+        },
         logData: function(){
             return data;
         }
@@ -33,7 +51,10 @@ const ItemCtrl = (function(){
 const UICtrl = (function(){
 
     const UISelectors = {
-        itemList: '#item-list'
+        itemList: '#item-list',
+        addBtn: '.add-btn',
+        itemNameInput: '#item-name',
+        itemCaloriesInput: '#item-calories'
     }
 
     return{
@@ -49,6 +70,15 @@ const UICtrl = (function(){
             });
             // Insert List Items 
             document.querySelector(UISelectors.itemList).innerHTML = html;
+        },
+        getItemInput : function(){
+            return {
+                name : document.querySelector(UISelectors.itemNameInput).value,
+                calories : document.querySelector(UISelectors.itemCaloriesInput).value
+            }
+        },
+        getSelector: function(){
+            return UISelectors;
         }
     }
 
@@ -57,6 +87,28 @@ const UICtrl = (function(){
 
 //App Controller
 const App = (function(ItemCtrl, UICtrl){
+
+    // Load event Listener 
+    const loadEventListners = function(){
+        //Get UI Selectors
+        const UISelectors = UICtrl.getSelector();
+        //Add item event
+        document.querySelector(UISelectors.addBtn).addEventListener('click',itemAddSubmit);
+
+
+    };
+
+    const itemAddSubmit = function(e){
+        //get form input from UIcontroller
+        const input = UICtrl.getItemInput();
+        if(input.name !== '' && input.calories !== ''){
+            const newItem = ItemCtrl.addItem(input.name,input.calories);
+        }
+
+         console.log("add");
+        e.preventDefault();
+    }
+
     //public methods
     return {
         init: function(){
@@ -65,6 +117,8 @@ const App = (function(ItemCtrl, UICtrl){
             const items = ItemCtrl.getItems();
             
             UICtrl.populateItemList(items);
+
+            loadEventListners();
             
         }
     }
